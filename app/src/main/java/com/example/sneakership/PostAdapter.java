cb586b193f4d6212fae92de86aa66afe9a0e5c04
abhishekvlog <abhishek.kumar.kps25@gmail.com>
2021-09-24 16:43:20 +0530

    Commit 3

--- app/src/main/java/com/example/sneakership/PostAdapter.java
+++ app/src/main/java/com/example/sneakership/PostAdapter.java
@@ -1,5 +1,6 @@
 package com.example.sneakership;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,21 +9,31 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PostAdapter extends RecyclerView.Adapter<PostViewHolder> {
+    private ArrayList<ResponseDTO> responseList;
+    public PostAdapter(ArrayList<ResponseDTO> responseList){
+        this.responseList = responseList;
+    }
     @NonNull
     @Override
     public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout,parent,false);
+        return new PostViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
-
+        holder.setData(responseList.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return responseList.size();
     }
 }
 class PostViewHolder extends RecyclerView.ViewHolder {
@@ -35,5 +46,12 @@ class PostViewHolder extends RecyclerView.ViewHolder {
         Name = itemView.findViewById(R.id.Name);
         Prize = itemView.findViewById(R.id.price);
     }
-    public setData()
+    public void setData(ResponseDTO responseDTO){
+        if (responseDTO.getMedia().getImageUrl() != null){
+            Glide.with(imageView).load(responseDTO.getMedia().getImageUrl());
+        }
+        Name.setText(responseDTO.getName());
+        Prize.setText(responseDTO.getRetailPrice());
+
+    }
 }
--- app/src/main/java/com/example/sneakership/SneakerFragment.java
+++ app/src/main/java/com/example/sneakership/SneakerFragment.java
@@ -2,15 +2,28 @@ package com.example.sneakership;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 public class SneakerFragment extends Fragment {
-
+    private RecyclerView recyclerView;
+    private ArrayList<ResponseDTO> responseList;
+    private PostAdapter postAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,5 +31,38 @@ public class SneakerFragment extends Fragment {
 
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+        setRecyclerView();
+        ApiCall();
+    }
 
+    private void ApiCall() {
+        ApiClient apiClient = Network.getRetrofitInstance().create(ApiClient.class);
+        apiClient.getShoe().enqueue(new Callback<List<ResponseDTO>>() {
+            @Override
+            public void onResponse(Call<List<ResponseDTO>> call, Response<List<ResponseDTO>> response) {
+             responseList.addAll(response.body());
+             postAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<ResponseDTO>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void setRecyclerView() {
+        postAdapter = new PostAdapter(responseList);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext() , 2);
+        recyclerView.setAdapter(postAdapter);
+        recyclerView.setLayoutManager(gridLayoutManager);
+    }
+
+    private void initView(View view) {
+        recyclerView = view.findViewById(R.id.recyclerView);
+    }
 }
