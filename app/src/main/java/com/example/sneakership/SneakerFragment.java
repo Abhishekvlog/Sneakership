8521863c624a07e2af834d819f6a76a77776e881
abhishekvlog <abhishek.kumar.kps25@gmail.com>
2021-09-24 15:45:50 +0530

    Initial commit 1

---
+++ app/src/main/java/com/example/sneakership/SneakerFragment.java
@@ -0,0 +1,22 @@
+package com.example.sneakership;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SneakerFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
